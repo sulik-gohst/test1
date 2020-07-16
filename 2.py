05386cae38eae889091c697e5852d9d21079af31
sulik-gohst <myrzakanovsultan0@gmail.com>
2020-07-16 20:09:37 +0600

    22

---
+++ 2.py
@@ -0,0 +1,11 @@
+def square_number(number):
+  return number**2
+#DO NOT remove lines below here,
+#this is designed to test your code.
+def test_square_number():
+  assert square_number(2) == 4
+  assert square_number(8) == 64
+  assert square_number(10) == 100
+  print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+test_square_number()
